83042bc3bdd51a926db27ad6fe44711b12a774ae
Derek Lewis <dlewis9442@gmail.com>
2022-10-03 20:51:41 -0500

    letters in word print to div

--- script.js
+++ script.js
@@ -3,24 +3,34 @@ let keyboard = document.querySelector('.keyboard')
 let word = document.querySelector('.word')
 let hiddenWord = 'bronco'
 let wordArray = []
+
 let turns = 10
 
-// let hiddenArray = () => {
-//   for (let i = 0; i < hiddenWord.length; i++) {
-//     wordArray.push(hiddenWord[i])
-//     console.log(wordArray)
-//   }
-// }
+let hiddenArray = () => {
+  for (let i = 0; i < hiddenWord.length; i++) {
+    wordArray.push(hiddenWord[i])
+    console.log(wordArray)
+  }
+}
 
-let game = () => {
+let letterSelect = () => {
   for (let i = 0; i < keys.length; i++) {
     keys[i].addEventListener('click', () => {
       let letter = keys[i].innerHTML.toLowerCase()
       console.log(letter)
-      word.innerHTML = letter
+      // word.innerHTML = letter
       // hiddenArray()
+      let result = wordArray.includes(letter)
+      console.log(result)
+      if (result == true) {
+        const newDiv = document.createElement('div')
+        newDiv.classList.add('letter')
+        newDiv.innerHTML = letter
+        word.appendChild(newDiv)
+      }
     })
   }
 }
 
-game()
+letterSelect()
+hiddenArray()
